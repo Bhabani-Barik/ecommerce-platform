8c880e26710fa80153cfcad15075c9c776f39755
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2026-08-15 16:56:54 +0530

    Implemented Backend Validation as well as custom exception handling

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,32 +28,23 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
-        //return "Category added successfully";
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable("categoryId") Long categoryId) {
-       try {
            String status = categoryService.deleteCategory(categoryId);
            //return new ResponseEntity<>(status, HttpStatus.OK);
            //return ResponseEntity.ok(status); // another way to handle response status code
            return ResponseEntity.status(HttpStatus.OK).body(status);
-       } catch (ResponseStatusException e) { // handling status code
-           return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-       }
     }
 
     @PutMapping("/public/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category,
                                                  @PathVariable Long categoryId) {
-      try {
           Category savedCategory = categoryService.updateCategory(category, categoryId);
           return new ResponseEntity<>("Category with category id: " + categoryId + " updated successfully.", HttpStatus.OK);
-      } catch(ResponseStatusException e){
-          return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-      }
     }
 }
