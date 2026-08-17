0e56be49a6b309000becc6076fe691fbc02d4bc9
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2026-08-17 17:06:27 +0530

    Implemented Soting in GetMapping API endpoint

--- sb-ecom/src/main/java/com/ecommerce/project/config/AppConstants.java
+++ sb-ecom/src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -1,6 +1,12 @@
 package com.ecommerce.project.config;
 
 public class AppConstants {
+
+    // Pagination
     public static final String PAGE_NUMBER = "0";
     public static final String PAGE_SIZE = "50";
+
+    // Sorting
+    public static final String SORT_CATEGORIES_BY = "categoryId";
+    public static final String SORT_DIR = "asc";
 }
