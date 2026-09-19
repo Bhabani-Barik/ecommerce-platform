b7c2e8c07585182622dbbb2808ec94e0bedc45d0
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2026-09-19 08:56:54 +0530

    Implemented custom UserDetailsService

---
+++ sb-ecom/src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.security.services;
+
+import com.ecommerce.project.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       // Fetching the userdetails from database using our user model/entity and return the object of our User Model
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        // Spring Security expects it in its user model or its UserDetails interface, we're returning object of its implementation class object.
+        return UserDetailsImpl.build(user);
+    }
+}
